4dcb3cf3184845790964687543cf4ad763bff67f
Jason Wang <vuwij@me.com>
2018-07-15 20:28:31 -0400

    Cleaned up files

--- loadRobot.m
+++ loadRobot.m
@@ -1,25 +1,10 @@
 % Load Paths
-rosshutdown;
 addpath('fixed_trajectories')
 addpath('../soccer-control')
 addpath('../soccer-vision')
 
-% IP Addresses for robots
-localIp = '192.168.2.14';
-gazeboIp = '192.168.2.54';
-robotIp = '172.20.10.4';
-
-useRobot = 0;
-
-if useRobot
-    setenv('ROS_IP', localIp)
-    setenv('ROS_MASTER_URI',strcat('http://', robotIp, ':11311'))
-    rosinit(robotIp, 'NodeHost', localIp)
-else
-    setenv('ROS_IP', localIp)
-    setenv('ROS_MASTER_URI',strcat('http://', gazeboIp, ':11311'))
-    rosinit(gazeboIp, 'NodeHost', localIp)
-end
+% Connect Robot
+connectRobot;
 
 % Generate Messages
 % rosgenmsg('soccer_msgs')
