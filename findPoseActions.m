e8e85c86273b83c0aebc204d160eb77184d81d40
Jason Wang <vuwij@me.com>
2018-08-18 17:45:37 -0400

    Errors Fixed

--- findPoseActions.m
+++ findPoseActions.m
@@ -1,15 +1,4 @@
-
-function trajectory = findPoseActions(curPose, destPose, obstacles)
-
-speed = 0.01; % m/s
-
-% curPose = Pose(0,0,0,0,0);
-% destPose = Pose(2,2.2,0,0,0);
-% 
-% obs1 = Pose(1.3,1.3,0,0,0);
-% obs2 = Pose(-1.7,1.7,0,0,0);
-% obs3 = Pose(1.5,-1.5,0,0,0);
-% obstacles = {obs1, obs2, obs3};
+function trajectory = findPoseActions(curPose, destPose, obstacles, speed)
 
 waypoints = findWayPoints(curPose, destPose, obstacles);
 grid on;
---
+++ findWayPointsTest.m
@@ -0,0 +1,11 @@
+curPose = Pose(0,0,0,0,0);
+destPose = Pose(2,2.2,0,0,0);
+
+obs1 = Pose(1.3,1.3,0,0,0);
+obs2 = Pose(-1.7,1.7,0,0,0);
+obs3 = Pose(1.5,-1.5,0,0,0);
+obstacles = {obs1, obs2, obs3};
+
+speed = 0.01; % m/s
+
+trajectory = findPoseActions(curPose, destPose, obstacles, speed);
